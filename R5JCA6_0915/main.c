f204c0bdacfdbfd878d22f5dd6cddfad149f6b5b
brankovicsb <55716735+brankovicsb@users.noreply.github.com>
2021-10-05 22:34:12 +0200

    Add files via upload

---
+++ R5JCA6_0915/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+    FILE *fp;
+    fp = fopen("munka.txt", "w");
+
+    char beolvas;
+    char szoveg[100];
+
+    printf("2. feldat: \n");
+    printf("Adja meg a szoveget: \n");
+
+    while( (beolvas = getchar()) != '#' )
+    {
+        putc(beolvas, fp);
+    }
+
+    fclose(fp);
+
+    fp = fopen("munka.txt", "r");
+
+    while( (beolvas = getc(fp)) != EOF )
+    {
+        printf("%c", toupper(beolvas) );
+    }
+
+    fclose(fp);
+
+    return 0;
+}
