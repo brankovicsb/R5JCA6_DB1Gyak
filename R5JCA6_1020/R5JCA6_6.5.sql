9c9e4b3b7f1c00090cb945cdf78d7b310a877246
brankovicsb <55716735+brankovicsb@users.noreply.github.com>
2021-10-25 19:42:47 +0200

    Create R5JCA6_6.5.sql

---
+++ R5JCA6_1020/R5JCA6_6.5.sql
@@ -0,0 +1,5 @@
+INSERT INTO termekek VALUES (1,’lapát’, 2000,’K1’); // lézető kulcs
+INSERT INTO termekek VALUES (8,NULL, 4000,’K1’); // üres név;
+INSERT INTO termekek VALUES (15,’pohár A’, 0,’K3’); // hibás ár
+INSERT INTO termekek VALUES (15,”pohár A”, 20,’K3’); // hibás szöveg konstans
+INSERT INTO termekek VALUES (15,’pohár A’, ‘20’,’K3’); // hibás szám
